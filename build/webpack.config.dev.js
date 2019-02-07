55acf3439bc3992ba51abce0824d7db30c5e6608
petusa <peter.x.nagy@gmail.com>
2019-02-07 14:56:11 +0400

    Initial commit for vue with webpack template
    - does not support Babel transpilation yet
    - static asset copy to dist folder is not tested yet
    - does not come with testing libraries (maybe later)
    - does not include prod build config yet
    - credits Daniel Cook, based on his articles (https://itnext.io/vuejs-and-webpack-4-from-scratch-part-1-94c9c28a534a)

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const path = require('path')
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    './src/app.js'
+  ],
+  devServer: {
+    hot: true,
+    watchOptions: {
+      poll: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|vue)$/,
+        use: 'eslint-loader',
+        enforce: 'pre'
+      },
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([{
+      from: resolve('static/img'),
+      to: resolve('dist/static/img'),
+      toType: 'dir'
+    }]),
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.css'
+    })
+  ]
+}
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="full-width center-content">
+    <hello-component name="Peter" />
+  </div>
+</template>
+
+<script>
+import HelloComponent from './components/HelloComponent.vue'
+
+export default {
+  components: {
+    HelloComponent
+  }
+}
+
+</script>
+
+<style scoped>
+.full-width {
+  width: 100%;
+}
+.center-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
